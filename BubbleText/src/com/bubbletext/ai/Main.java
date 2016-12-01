b0196de8b96bacfdea2a4908285994e7e4ff72f6
sydpy <sylvain.dupouy@etu.u-bordeaux.fr>
2016-12-01 20:35:58 +0100

    Hellow world !

---
+++ BubbleText/src/com/bubbletext/ai/Main.java
@@ -0,0 +1,12 @@
+package com.bubbletext.ai;
+
+/**
+ * Created by Sylvain DUPOUY on 12/1/16.
+ */
+public class Main {
+
+    public static void main(String args[]){
+        System.out.println("Hello world !");
+    }
+
+}
